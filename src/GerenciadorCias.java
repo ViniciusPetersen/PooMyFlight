771784b6f3785783d07587cbc0513fc18e2c9eda
ViniciusPetersen <viniciuspetersen03@gmail.com>
2023-03-28 15:42:05 -0300

    tentativa 3 add metodos GerenciadorCias

--- src/GerenciadorCias.java
+++ src/GerenciadorCias.java
@@ -1,9 +1,22 @@
 import java.util.ArrayList;
 
 public class GerenciadorCias {
-    private ArrayList<CiaAerea>empresas;
+    private ArrayList<CiaAerea> empresas;
     private GerenciadorCias(){
         empresas = new ArrayList<>();
+    }
+
+    public void adicionar(CiaAerea cia){
+        empresas.add(cia);
+    }
+
+    public CiaAerea buscarPorcodigo(String cod){
+        for (int i=0; i<empresas.size(); i++){
+            CiaAerea a = empresas.get(i);
+            if(a.getCodigo().equals(cod))
+            return a;
+        }
+        return null;
     }
 
     public static GerenciadorCias instance;
