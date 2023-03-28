f340656a66e63e285d40b9bf57408fe6a67bafc7
AlexiLeonardo <alexi.leonardo@gmail.com>
2023-03-28 16:44:49 -0300

    Mudança de método

--- src/GerenciadorCias.java
+++ src/GerenciadorCias.java
@@ -10,7 +10,7 @@ public class GerenciadorCias {
         empresas.add(cia);
     }
 
-    public CiaAerea procuraPorcodigo(String cod){
+    public CiaAerea procurarPorCodigo(String cod){
         for (int i=0; i<empresas.size(); i++){
             CiaAerea a = empresas.get(i);
             if(a.getCodigo().equals(cod))
