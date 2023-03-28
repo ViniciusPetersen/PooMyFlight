d0eaaadf46be42528af0612e9954728fa2bbff21
ViniciusPetersen <viniciuspetersen03@gmail.com>
2023-03-28 16:03:00 -0300

    add metodo adicionar GerenciadorRotas, ajuste.giti

--- src/GerenciadorRotas.java
+++ src/GerenciadorRotas.java
@@ -2,11 +2,16 @@ import java.util.ArrayList;
 
 public class GerenciadorRotas {
 
-    private ArrayList<GerenciadorRotas>rotas;
+    private ArrayList<Rota>rotas;
     private GerenciadorRotas(){
         rotas = new ArrayList<>();
     }
     private Aeronave aeronave;
+
+    public void adicionar(Rota rota){
+        rotas.add(rota);
+    }
+
 
     public static GerenciadorRotas instance;
     public static GerenciadorRotas getInstance(){
