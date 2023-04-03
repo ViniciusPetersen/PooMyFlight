d2e8a1088701693adbbdb4fe06793cdb796b6ac3
Adã Cardoso <112208373+acarddoso@users.noreply.github.com>
2023-04-03 15:33:46 -0300

    Update VooEscalas.java
    toString atualizado

--- src/VooEscalas.java
+++ src/VooEscalas.java
@@ -1,21 +1,16 @@
 import java.time.Duration;
 import java.time.LocalDateTime;
-
-public class VooEscalas extends Voo
-{
+public class VooEscalas extends Voo{
     private Rota rotaFinal;
-
-    // Construtor
     public VooEscalas(Rota rota, Rota rotaFinal, LocalDateTime datahora, Duration duracao) {
-        super(datahora, duracao,rota); // chama o construtor de Voo
+        super(datahora, duracao, rota);
         this.rotaFinal = rotaFinal;
      }
-
-    public Rota getRotaFinal() { return rotaFinal; }
-	...
-
-    @Override
-    public String toString() {
-       return status + " " + datahora + "("+duracao+"): " + rota + " -> " + rotaFinal; // ERRO!
-    }
+     public Rota geRotaFinal(){
+        return rotaFinal;
+     }
+        @Override
+     public String toString() {
+        return super.toString() + " -> " + rotaFinal;
+     }
 }
